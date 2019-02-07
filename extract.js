7e1934b719218d057469ab90b2eb3baca64bf8ca
Marin <adminy@Marins-iMac.local>
2019-02-07 23:38:56 +0000

    Generating proper json now from mkvinfo output. also fixed string indexes, also bonus language feature

--- extract.js
+++ extract.js
@@ -1,4 +1,5 @@
 let fs = require('fs'),
+	languages = require("./languages.json"),
     tracks = []
 
 fs.readFile('input.txt', 'utf8', function(err, contents) {
@@ -40,17 +41,47 @@ fs.readFile('input.txt', 'utf8', function(err, contents) {
 		if(processingTrack == 2 && lines[i].substring(0, 5) == "|  + ") {
 			let data_info = lines[i].substring(5)
 			
-			if( data_info.indexOf("Track number:") !== -1 ||
-			    data_info.indexOf("Track type:") !== -1 ||
-			    data_info.indexOf("Codec ID:") !== -1 ||
-			    data_info.indexOf("Name:") !== -1 ||
-			    data_info.indexOf("Language:") !== -1 )
+			if( data_info.indexOf("Track number:") !== -1 )
+				track.id = data_info.substring(13).split(":")[1].split(")")[0].substring(1)
+			
+			else if( data_info.indexOf("Track type:") !== -1 )
+				track.type = data_info.substring(12).split()[0]
+			
+			else if( data_info.indexOf("Codec ID:") !== -1 )
+				track.codec = data_info.substring(10).split()[0]
 
-				console.log(data_info)
-			//TODO: Actually append this data to tracks, not just print it ...
+			else if( data_info.indexOf("Name:") !== -1 )
+				track.name = data_info.substring(6).split()[0]
+
+			else if( data_info.indexOf("Language:") !== -1 && track.type !== "video" )
+				 track.language = data_info.substring(10).split()[0]
+			
+			//bonus, if track name mentions language in it ...
+			if( track.name && !track.language )
+				for(let i = 0; i < languages.length; i++)
+					if(track.name.indexOf(languages[i][1]) !== -1)
+						track.language = track.languages[i][0]
+
 		}
+		
+		//video resolution information (part of the track)
+		if(processingTrack == 2 && lines[i].substring(0, 6) == "|   + ") {
+			let data_info = lines[i].substring(6)
+			
+			if( data_info.indexOf("Display width:") !== -1 )
+				track.width = data_info.substring(15)	//you can wrap this in a parseInt ... but not neccesary
+				
+			else if( data_info.indexOf("Display height:") !== -1 )
+				track.height = data_info.substring(16)	//and this
+
+			
+		}
+		// console.log(lines[i])
 
 		
 	}
+
+	//Finished getting tracks information, now onto extracting the necessary ones
+	console.log(tracks)
 
 })
